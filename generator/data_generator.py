704f322ba65c6e302eac1c92f05c1f3178999d90
Vinasque <b51986@fgv.edu.br>
2025-04-04 17:44:53 -0300

    random json data for example done

---
+++ generator/data_generator.py
@@ -0,0 +1,38 @@
+import json
+import random
+from faker import Faker
+from datetime import datetime, timedelta
+
+faker = Faker()
+Faker.seed(42)
+
+def generate_flight_orders(n):
+    orders = []
+
+    for _ in range(n):
+        # Escolhendo dados aleatoriamente
+        flight_id = random.choice([f"AAA-{i}" for i in range(1, 31)])
+        seat = f"{random.randint(1, 40)}{random.choice(['A', 'B', 'C', 'D', 'E', 'F'])}"
+        user_id = str(random.randint(10000, 99999))
+        customer_name = faker.name()
+        status = random.choice(["pending", "confirmed", "cancelled"])
+        payment_method = random.choice(["credit_card", "debit_card", "paypal"])
+        reservation_time = faker.date_time_between(start_date = "-60d", end_date = "now").isoformat()
+
+        order = {
+            "flight_id": flight_id,
+            "seat": seat,
+            "user_id": user_id,
+            "customer_name": customer_name,
+            "status": status,
+            "payment_method": payment_method,
+            "reservation_time": reservation_time
+        }
+
+        orders.append(order)
+
+    return orders
+
+orders = generate_flight_orders(500) 
+with open("orders.json", "w") as f:
+    json.dump(orders, f, indent=2)
